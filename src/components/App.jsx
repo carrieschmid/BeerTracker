57cc11886109f0b2f1f2f36fcb7639668768313c
Carrie Schmid <carriepederson7@gmail.com>
2019-11-22 08:11:53 -0800

    Take out Popup.js stuff.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import Login from './Login';
 import AddKeg from './AddKeg';
 import { Switch, Route } from 'react-router-dom';
 // import React, { Component } from 'react';  
-// import Popup from './components/Popup';
+
 
 
 
@@ -25,36 +25,4 @@ function App() {
 }
 
 export default App;
-
-// class App extends Component {  
-
-//     constructor(props){  
-//   super(props);  
-//   this.state = { showPopup: false };  
-//   }  
-  
-//     togglePopup() {  
-//   this.setState({  
-//        showPopup: !this.state.showPopup  
-//   });  
-//    }  
-  
-//     render() {  
-//   return (  
-//   <div>  
-//   <h1> Simple Popup Example In React Application </h1>  
-//   <button onClick={this.togglePopup.bind(this)}> Click To Launch Popup</button>  
-  
-//   {this.state.showPopup ?  
-//   <Popup  
-//             text='Click "Close Button" to hide popup'  
-//             closePopup={this.togglePopup.bind(this)}  
-//   />  
-//   : null  
-//   }  
-//   </div>  
-  
-//   );  
-//   }  
-//   }  
 
