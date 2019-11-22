8f36157164c319776cc9e84bcacf0915617f2225
Carrie Schmid <carriepederson7@gmail.com>
2019-11-22 14:13:40 -0800

    Add code to Admin.jsx

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import OurBeer from './OurBeer';
-import BeerProfile from './BeerProfile';
 import BeerDetail from './BeerDetail';
 
 function Admin(props) {
