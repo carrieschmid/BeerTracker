be4295a622453ef689b9deabfc993a222c05af24
Carrie Schmid <carriepederson7@gmail.com>
2019-11-24 20:24:09 -0800

    Update NewKegControl.jsx.

--- src/components/NewKegControl.jsx
+++ src/components/NewKegControl.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
-
+import ConfirmationQuestions from './ConfirmationQuestions';
 import AddKeg from './AddKeg';
 import PropTypes from 'prop-types';
 
 class NewKegControl extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          formVisibleOnPage: false
-        };
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+    this.handleTroubleshootingConfirmation = this.handleTroubleshootingConfirmation.bind(this);
+  }
+
+  handleTroubleshootingConfirmation(){
+    this.setState({formVisibleOnPage: true});
+  }
 
   render(){
     let currentlyVisibleContent = null;
     if (this.state.formVisibleOnPage){
-        currentlyVisibleContent = <AddKeg onAddNewKeg={this.props.onAddNewKeg}/>;
-      } else {
-        currentlyVisibleContent = "Please enter."
-      }
+      currentlyVisibleContent = <AddKeg onAddNewKeg={this.props.onAddNewKeg}/>;
+    } else {
+      currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation}/>;
+    }
     return (
+      <div>
         {currentlyVisibleContent}
+      </div>
     );
   }
 }
@@ -28,4 +35,4 @@ NewKegControl.propTypes = {
   onAddNewKeg: PropTypes.func
 };
 
-export default NewKegControl;
+export default NewTicketControl;
